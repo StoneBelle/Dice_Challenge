b2909d292980938c4efd83ecb4fd966ae70cbffd
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-10-01 21:29:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-
 //Generates a random number for each player
 randomNumber1 = Math.random();
 randomNumber1 = randomNumber1 * 6;  
 randomNumber1 = Math.floor(randomNumber1);
 randomNumber1 ++ ;
 
+randomNumber2 = Math.floor(Math.random() * 6 ) + 1;
 
-randomNumber2 = Math.floor(Math.random() * 6 ) + 1
-
-// Changes image of dice for each player
+// Creates the image name to be inserted 
 let p1_dice = "images/dice" + randomNumber1 + ".png";
 let p2_dice = "images/dice" + randomNumber2 + ".png";
 
+// Changes image of dice for each player
 document.querySelector('.img1').setAttribute("src", p1_dice);
 document.querySelector('.img2').setAttribute("src", p2_dice);
 
-
 // let image1 = document.querySelector("img")[0];
 // image1.setAttribute("src", p1_dice);
+
+// Check which player has a greater dice roll & announce winner
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "🍊Player 1 Wins!";   
+} else if( randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = "Player 2 🍓 Wins!";   
+} else {
+    document.querySelector('h1').innerHTML = "It's a Draw!";   
+}
